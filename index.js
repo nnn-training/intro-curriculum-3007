cd9f26168d5cf44d890699a7ec79ee19dae34b46
smdktr <smdktr86@gmail.com>
2021-04-10 20:14:23 +0900

    seventeen モジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * 17 の倍数であれば、trueを返す関数
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
 
 module.exports = {
-}
+  isMultipleOfSeventeen,
+};
