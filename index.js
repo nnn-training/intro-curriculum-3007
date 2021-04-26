403144c59f43c200b5e0802308c609b3674e8d4a
Yumi <terayumim3113@gmail.com>
2021-04-26 14:09:06 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
+/**
+ * 配列に含まれる整数を17で割り切れるものだけにする
+ * @param{number} number
+ */
+function isMultipleOfSeventeen(number){
+   return number%17===0   
 }
+module.exports = {
+    isMultipleOfSeventeen
+};
