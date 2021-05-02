a6c410c88f4a20d3e02586dcb541845f5ecf2cee
pome-ta <pometarou93@gmail.com>
2021-05-02 21:53:54 +0900

    fix

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 'use strict';
 
+
+/**
+ * 整数が 17 で割り切れるかどうかを判定
+  * @param {number} num
+ */
+
+function isMultipleOfSeventeen(num) {
+  if (num % 17 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
 module.exports = {
+  isMultipleOfSeventeen
 }
