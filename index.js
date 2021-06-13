ec8c2f71834a6b9b07c45470f0293b9910a408fa
Tomohiro Aoki <aoki@Tomohiro-no-MacBook-Pro.local>
2021-06-13 11:07:20 +0900

    17で割り切れるものだけを抽出するモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17で割り切れる数の配列を取得する
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen }
