5e06af93dec8fc90850db665c0e1c4204cff211a
kyoruni <kyoruni@gmail.com>
2021-06-30 17:50:15 +0900

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 17の倍数の場合、trueを返す
+ * @param {number} num
+ * @returns 
+ */
+function isMultipleOfSeventeen (number) {
+  return number % 17 === 0
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
