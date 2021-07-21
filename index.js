e0542b2675b63b7b09e0426e6fa41270bcd606c4
Satoshi Nakagawa <nakasen20th@gmail.com>
2021-07-21 13:26:29 +0900

    17で割り切れるかどうかを返す関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
+const number = 0;
+
+/**
+ * 引数が 17 で割り切れるかどうかを返す
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(number) {
+  return number % 17 === 0;
+}
 
 module.exports = {
+  isMultipleOfSeventeen
 }
