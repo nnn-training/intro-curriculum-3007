15936a5d3e4e89325309ac02afba3a02042b048d
mizusawa.kazuyasu <kazu.mizu.tr@gmail.com>
2021-08-15 17:14:45 +0900

    isMultipleOfSeventeenを実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17の倍数の場合、true を返す
+ * @oaram {number} num
+ */
+function isMultipleOfSeventeen (num) {
+    return num % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
