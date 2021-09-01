dcb2f75541442c91d2ab0d35e61f804df850194a
kou-higashi <mortalstore2011@gmail.com>
2021-09-01 21:46:00 +0900

    isMultipleOfSeventeenの実装

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
 
+/**
+ * 
+ * @param {integer} digit 
+ */
+function isMultipleOfSeventeen(digit){
+    if(digit % 17===0){
+        return true;
+    }
+
+    return false;
+}
 module.exports = {
+    isMultipleOfSeventeen
 }
