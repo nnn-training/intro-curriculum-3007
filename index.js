79ca5139fe00346bad2769be9e36d4dc3f5602ae
fjtsh24 <mbd6en3jbr@i.softbank.jp>
2021-09-04 00:05:25 +0900

    isMultipleOfSeventeen 関数を seventeen モジュールに実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/*
+ * 入力された整数が 17 で割り切れるかどうかを返す
+ * @param {number} num
+ * @return {boolean}
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0
+}
 
 module.exports = {
+    isMultipleOfSeventeen
 }
