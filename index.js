eadb5d2c07384edf54ab8b6637843132e20f5e97
小笠原徳和 <ogasawaranorikazu@ogasawaranorikazunoMacBook-Air.local>
2021-09-11 15:17:20 +0900

    もう大丈夫。治った！

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 // 配列に含まれる整数が 17 で割り切れるものだけにする
 
-function isMultipleOfSeventeeen(num) {
+function isMultipleOfSeventeen(num) {
   return num % 17 === 0;
 }
 
 module.exports = {
-  isMultipleOfSeventeeen,
+  isMultipleOfSeventeen
 }
