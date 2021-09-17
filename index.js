800ee19684e565e34764f01c25f5279a00d0f173
hana fleur <yukay2184@gmail.com>
2021-09-17 18:03:44 +0900

    17の倍数にtrueを返す関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
-module.exports = {
+
+/**
+ * テストが配列メソッドの .filter なので、17の倍数であればtrueを返す関数を作成
+ * @param {int} number 
+ */
+function isMultipleOfSeventeen (number){
+  return number % 17 === 0;
 }
+module.exports = { isMultipleOfSeventeen
+};
