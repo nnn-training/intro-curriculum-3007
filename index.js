daa56e2e3a41acf786685e91b28da98ed3d8bf91
mono0048 <morikeisuke48@gmail.com>
2021-09-22 16:54:04 +0900

    試作 未検証

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 'use strict';
+/**
+ * 配列に含まれる整数が17で割り切れるものだけにする
+ * つまり、17で割り切れないものを削除すればよい？
+ * 読込、判定、削除or戻し。
+ * @param {integer} integer
+ */
+// seventeen.isMultipleOfSeventeen
+const integers = [];
+function isMultipleOfSeventeen(integer) {
+    for (const i = 0; i <= integers.length; i++) {
+        if (integers[i] % 17 !== 0) {
+            integers[i].splice(integers[i], 1);
+        }
+    }
+}
+
+
+
+
+
 
 module.exports = {
 }
