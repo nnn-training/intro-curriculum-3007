a607102020f2f2d8e96bcd929a9691ed37d60cb5
Fuuchin <denkisutandokese@outlook.jp>
2021-09-23 16:45:36 +0900

    配列に含まれる整数を17 で割り切れるものだけにした。

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
-module.exports = {
+/**
+ * test.jsの配列のうち、17で割り切れるものだけを返す関数
+ * @param {number} n 
+ * @returns 17の倍数
+ */
+function isMultipleOfSeventeen(n) {
+  return n % 17 === 0;
 }
+
+module.exports = {
+  isMultipleOfSeventeen
+};
