e4727238b417dd0e4fb7dbd078fda7a17e43d1d8
Atsunori Hattori <tihove7k@gmail.com>
2021-09-26 11:46:47 +0900

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
+function isMultipleOfSeventeen(n) {
+    if (n % 17 === 0) {
+        return true;
+    }
+    return false;
+};
+
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
