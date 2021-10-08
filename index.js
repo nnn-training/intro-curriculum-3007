95b63c372e4728d15eb0d9868b136789a393a567
Akio Watahiki <akio.watahiki@gmail.com>
2021-10-09 03:14:50 +0900

    １７で割り切れる数のみを返す関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
+/**
+ * 17で割り切れる数のみを返す
+ * @param {number} n
+ */
+ function isMultipleOfSeventeen(n) {
+    return n % 17 === 0;
+} 
 module.exports = {
+    isMultipleOfSeventeen
 }
