28db342fb306ab590a4a14f8143d57f552afc2bd
Shoma SUZUKI <suzu.shouma@gmail.com>
2021-10-16 20:06:29 +0900

    17で割り切れるかどうかを判定する関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * return true if the number is a multiple of 17
+ * @param {number} num 
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
