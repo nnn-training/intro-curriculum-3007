b2e4389fc723ecfec571ffe2626ee75e0a233100
Yuto Moriyasu <y.moriyasu@icloud.com>
2021-11-24 01:33:18 +0900

    isMultipleOfSeventeenを実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+function isMultipleOfSeventeen(int) {
+  if (int % 17 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
