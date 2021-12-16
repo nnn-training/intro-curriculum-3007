dcfc4a4e410bd1b8e55735dc0df8ae8286e50639
nakako <nakako.soft@gmail.com>
2021-12-16 23:29:04 +0900

    17の倍数かを判定する関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17の倍数かを判定する(true: 17の倍数, false: 17の倍数以外)
+ * @param {number} num 判定する値
+ * @returns 判定結果
+ */
+function isMultipleOfSeventeen(num){
+  return num % 17 === 0;
 }
+
+module.exports = {
+  isMultipleOfSeventeen
+};
