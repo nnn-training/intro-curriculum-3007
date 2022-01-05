a924b341921d9d922a92f01ec013c00eb54cbc4d
Kota Shimizu <s.kouta0723@outlook.jp>
2022-01-06 01:31:45 +0900

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * 17の倍数であるときtrueを返す
+ * @param {number} num 
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
