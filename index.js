720c6a2e7459fb21a44c401ee14cdd94507f4847
米田祐至 <yujiyoneda@yonedayuuitarinoMac-mini.local>
2022-01-14 15:19:18 +0900

    isMultipleOfSeventeenメソッドの実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
-
+/** 
+ * 配列に含まれる整数が17で割り切れるものだけにする
+ * filterメソッドを使うので戻り値はtrue/false
+ * @param {numbers}  判定する値
+ * @return true or false
+ */
+function isMultipleOfSeventeen(numbers){
+    return numbers % 17 === 0;    
+}
 module.exports = {
+    isMultipleOfSeventeen
 }
