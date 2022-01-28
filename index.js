6443f1594b86130fef699a99e82f5c3c5ddbd5df
winkamu <intakamu@gmali.com>
2022-01-28 18:56:17 +0900

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 17で割り切れる数字を出力する
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
