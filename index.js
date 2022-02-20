d9dfd7303cf871eefac4a8654164dc3c0bbd4ade
nononon <kagura.mellow@gmail.com>
2022-02-20 17:47:38 +0900

    17の倍数を検索する

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * 配列の中から17で割り切れるものだけを抽出する、マップ関数で抽出した数字のみで新しい配列を作成する
+ * @param {number} num
+ * @return true or false
+ */
+function isMultipleOfSeventeen(num){
+  return num%17 === 0;
+}
 
-module.exports = {
+module.exports = { isMultipleOfSeventeen
 }
