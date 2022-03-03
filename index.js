374c60feb76b718ea82daabb010adbe2a39b4034
N-hotate <sadalsuud.lowenzahn317@gmail.com>
2022-03-03 19:18:15 +0900

    isMultipleOfSeventeen関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17で割り切れる数を抽出する
+ * @param {number} num 
+ * @returns 17の倍数
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
 }
+
+
+module.exports = {
+    isMultipleOfSeventeen
+};
