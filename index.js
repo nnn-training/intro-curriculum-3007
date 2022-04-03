7245646cd69996911427b066dc879e58b4657d74
山田一繁 <yamadakazushige@mail.bbexcite.jp>
2022-04-03 20:16:33 +0900

    isMultipleOfSeventeen関数実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * 整数が 17 で割り切れるかどうかを判定する
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
 }
+
+module.exports = {
+  isMultipleOfSeventeen
+};
