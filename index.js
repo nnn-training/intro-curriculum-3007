55b28b797b8ef0ba80a2a34e7abc4340fbdc2762
ShokiYokota <102781089+ShokiYokota@users.noreply.github.com>
2022-04-08 13:53:18 +0900

    間違えていたので修正しました

--- index.js
+++ index.js
@@ -3,11 +3,9 @@
 const a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60, 98, 38, 28, 96, 32];
 
 function isMultipleOfSeventeen (num){
-    if(num % 17 === 0){
-        return num;
-    }
+    return num % 17 === 0;
 }
-//console.log(this.isMultipleOfSeventeen(a));
+
 
 
 module.exports = {
