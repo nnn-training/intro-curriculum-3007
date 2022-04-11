80a5cd83a5bce188ebf5230e6fbf9c40cbe3783c
Takayuki Ogawa <sora758@me.com>
2022-04-12 05:44:15 +0900

    isMultipleOfSeventeen 関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17の倍数の時　tureを返す関数
+ * @param {number} num 
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
 }
+
+module.exports = {
+  isMultipleOfSeventeen
+};
