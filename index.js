03590c3c610a0f8ff9b91a76921ecf0e7598ee1f
musou-grass <musougrass@gmail.com>
2022-04-13 20:16:34 +0900

    17の倍数

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 17の倍数である場合 true を返す
+ * @param {number} num
+ */
+
+function isMultipleOfSeventeen() {
+  return arguments[0] % 17 === 0;
+}
 
 module.exports = {
-}
+  isMultipleOfSeventeen
+};
