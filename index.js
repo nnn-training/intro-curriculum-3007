b0732615548385baa4919bcd605deaf7394359a8
hamusuke0320 <nekonekobari@gmail.com>
2022-04-15 11:56:58 +0900

    seventeen モジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
 
+/**
+ * 17で割り切れる値の場合、trueを返す
+ * @param {number} numbers 
+ * @returns 
+ */
+function isMultipleOfSeventeen(numbers) {
+    if (numbers % 17 === 0) {
+        return true;
+    };
+};
+
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
