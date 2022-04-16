5d37eacd28f290330c07ee0ca6af854c788cdb6a
Kouhei Hamano <marco.fronsys@gmail.com>
2022-04-17 00:44:20 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 'use strict';
 
-module.exports = {
+
+//自身の回答
+
+// function isMultipleOfSeventeen(numList) {
+//   if ( numList % 17 === 0){
+//     let number = numList;
+//     return number;
+//   }
+// }  
+
+// 模範解答
+/**
+ * 17の倍数である場合 true を返す
+ * @param {number} num
+ */
+ function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
 }
+
+module.exports = { isMultipleOfSeventeen };
