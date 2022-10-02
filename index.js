2c040ffe81f96fb9ab989cf2f99eba453b959eb5
ry0922 <ry2922@icloud.com>
2022-10-02 12:13:35 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
 
+/**
+ * 17の倍数である場合trueを返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num){
+    if(num%17===0){
+        return true;
+    }
+    return false;
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
