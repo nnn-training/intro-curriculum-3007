b94028277fc82fa238b47a95a9f04ae787ad988b
fool-hy <gmirionman@gmail.com>
2023-02-08 16:46:22 +0900

    練習問題

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
 
-module.exports = {
+const a = [83, 32, 85, 47, 77, 8, 61, 74, 29, 34, 11, 76, 60, 99, 55, 7, 19, 60, 98, 38, 28, 96, 32];
+
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
 }
+
+// function isMultipleOfSeventeen(number) {
+//     return number
+//       .filter(num => num % 17 === 0 );
+// }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
