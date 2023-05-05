23362ee5d7787966d4ebfc089f9654d712dc6551
Kyosuke Taniguchi <kyosuke.taniguchi.s2@gmail.com>
2023-05-05 19:52:26 +0900

    17倍数判定機能の実装

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17の倍数である場合 true を返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num){
+    if(num % 17 === 0){
+        return true;
+    }
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
