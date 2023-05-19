e3bfc6c1db0c16a81fac1feeb122893dff3e1d77
core <kodaira1315@gmail.com>
2023-05-19 22:31:32 +0900

    isMultipleOfSeventeen関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
 
-module.exports = {
-}
+/**
+ * 17の倍数である場合trueを返す
+ * @param {number} num 
+ * @return - 17の倍数である場合true
+ */
+const isMultipleOfSeventeen = (num => num % 17 === 0);
+
+module.exports = { isMultipleOfSeventeen };
