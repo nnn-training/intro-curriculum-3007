6a62aac4146f1515c4935cf3a199fcfb2a39f7c0
chambear <chambear@my-macbook.local>
2023-08-26 09:55:43 +0900

    17の倍数確認する関数追加

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
 
+/**
+* 17の倍数である場合にtrueを返す
+* @param{number} num
+*/
+
+function isMultipleOfSeventeen(number) {
+  if (number % 17 === 0) {
+    return ('true');
+  }
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
