c5ad7430ee47832bd14736367706c29c5e2d335b
Susumu Otsuka <susumu.otsuka@gmail.com>
2025-02-19 18:28:19 +0900

    isMultipleOfSeventeen 関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/**
+ * 17 の倍数である場合 ture を返す
+ * @param {num} num
+ * @returns {boolean} 
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0;
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
