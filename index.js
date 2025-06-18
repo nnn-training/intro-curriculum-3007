9171c2d40120f0aa7a7267a72913fa228942dbad
kawasakinwwolf <kawasaki900rninja@gmail.com>
2025-06-18 20:34:38 +0900

    引数が 17 の倍数の場合に true を返す isMultipleOfSeventeen 関数の実装

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
+/**
+ * 
+ * @param {number} num 
+ * @returns {boolean}
+ */
+function isMultipleOfSeventeen(num){
+  if(num % 17 === 0){ 
+    return true };
+}
+
 module.exports = {
+  isMultipleOfSeventeen
 }
