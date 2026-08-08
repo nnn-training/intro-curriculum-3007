797695423473e39da5a0756cc57ddbbd67b7ee60
TUF_FUJIKEN <puripuriproject13@gmail.com>
2026-08-08 17:52:34 +0900

    Seventeen module 実装

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
-module.exports = {
+const boolean = [];
+
+
+
+/**
+ * @param { number } number
+ * @returns {boolean}
+ */
+
+function isMultipleOfSeventeen (num){
+  if (num%17 === 0 ) return true; //17の倍数でTRUE
 }
+
+module.exports = {
+  isMultipleOfSeventeen
+};
